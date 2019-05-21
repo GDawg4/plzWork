1093a9c96fea33351f3ea644211f4c62946001a9
GDawg4 <garoz.r4@gmail.com>
2019-05-21 14:25:20 -0600

    LastLab

---
+++ lastLab/lastLab/Assets/RoomScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomScript : MonoBehaviour
+{
+    Light redLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        redLight = GetComponent<Light>();
+        redLight.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (redLight.enabled)
+        {
+            redLight.intensity = 50 * Mathf.Sin(Time.time * 2) + 3;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "FPSController" || other.gameObject.name == "FirstPersonCharacter")
+        {
+            Debug.Log("Hes in the room");
+            redLight.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "FPSController" || other.gameObject.name == "FirstPersonCharacter")
+        {
+            Debug.Log("Not Anymore");
+            redLight.enabled = false;
+        }
+    }
+}
